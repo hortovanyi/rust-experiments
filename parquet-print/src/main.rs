ce6d1e8011ca809e7e72b9f0f79fabd90d5f2cd8
Nick Hortovanyi <nick.hortovanyi@gmail.com>
2020-03-21 15:10:26 +1000

    initial parquet-print

---
+++ parquet-print/src/main.rs
@@ -0,0 +1,20 @@
+extern crate parquet;
+
+use std::fs::File;
+use std::path::Path;
+use parquet::file::reader::{FileReader, SerializedFileReader};
+
+fn print_parquet(path:&str ) {
+    let file = File::open(&Path::new(path)).unwrap();
+    let reader = SerializedFileReader::new(file).unwrap();
+    let mut iter = reader.get_row_iter(None).unwrap();
+
+    while let Some(record) = iter.next() {
+        println!("{}", record);
+    }
+}
+fn main() {
+    print_parquet("../data/parquet-arrow-nav-sat-fix-20200318132301.parquet"); 
+}
+
+
